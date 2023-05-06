444e0822331e66eff7746abd36ddcb075b0f9d16
Ednan <52186220+ednanf@users.noreply.github.com>
2023-05-06 10:43:10 -0300

    Outlets created

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+// MARK: - IBOutlets
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
 
+    
+// MARK: - Properties
+    
+    
+// MARK: - Setup
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
+    
+    
+// MARK: - IBActions
+    
+    
+// MARK: - Functions
 
+    
 
 }
 
