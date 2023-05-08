94f47cf333957d6bd45069c041002c63728f91d9
Ednan <52186220+ednanf@users.noreply.github.com>
2023-05-08 12:44:37 -0300

    Project completed

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -18,8 +18,16 @@ class ViewController: UIViewController {
 // MARK: - Properties
     var listOfWords = ["bucaneer", "swift", "glorious", "incandescent", "bug", "program"]
     let incorrectMovesAllowed = 7
-    var totalWins = 0
-    var totalLosses = 0
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
     var currentGame: Game! // Holds the current game's value so it can be updated.
     
 // MARK: - Setup
@@ -35,21 +43,28 @@ class ViewController: UIViewController {
     
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false // Disables whichever button was pressed.
-        let letterString = sender.configuration!.title! // ATTENTION! I did not update letterStirng like page 377 instructed, because it crashes
+        let letterString = sender.configuration!.title! // ATTENTION! I did not update letterStirng like page 377 instructed, because it crashes the app!
         let letter = Character(letterString.lowercased()) // Converts the value from the line above to lowercase and change the type from String to Character.
         currentGame.playerGuessed(letter: letter) // Adds the letter contained in the button pressed to the method, in order to reduce the incorrectMovesRemaining.
-        updateUI()
+        updateGameState()
     }
     
 // MARK: - Functions
 
+    
+    // Starts a new round.
     func newRound() {
-        let newWord = listOfWords.removeFirst() // Removes **and returns** the first item.
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: []) // Sets the game with the removed word in the line above and sets the number of turns remaning to be the same number defined in the constant.
-        updateUI()
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst() // Removes **and returns** the first item.
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: []) // Sets the game with the removed word in the line above and sets the number of turns remaning to be the same number defined in the constant.
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+        }
     }
 
-    // Handles interface updates
+    // Handles interface updates.
     func updateUI() {
         correctWordLabel.text = currentGame.formattedWord // Sets the text of the label to match the text produced by the current Game struct using the property formattedWord.
         scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
@@ -63,6 +78,25 @@ class ViewController: UIViewController {
         let wordWithSpacing = letters.joined(separator: " ") // Joins all elements in the array, separating them with a space.
         correctWordLabel.text = wordWithSpacing
     }
+    
+    // Counts the amount of wins and losses.
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    
+    // Enable the letter buttons back after a round is finished.
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+    
     
 }
 
